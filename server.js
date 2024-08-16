895c4c2f95ff486798e5cb0edee555ae1ffe136d
dstrzykalski <dstrzykalski@yahoo.com>
2024-08-15 20:41:08 -0400

    Changed File Structure

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('microphone check');
 }) 
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
